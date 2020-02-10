a0fbf6e09fdc3279cca9396bb42e3d38b4477a89
James Elson <jameselson19@gmail.com>
2020-02-10 14:46:08 +0000

    modified pages and page layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `IP Project Title`,
+    description: `This is the page description for the IP project.`,
+    author: `IP Group 18`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
