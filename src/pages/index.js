db9289feda4ff075e091c1aa78e00ce4f58112af
Felix <felixmasters15@gmail.com>
2020-02-11 19:31:49 +0000

    Test commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Welcome</h1>
     <p>This is some test text for the index page</p>
+      <h2>Test</h2>
 
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
