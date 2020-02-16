33078ce4ef966c3942472dac2710f39e02460c4a
James Elson <jameselson19@gmail.com>
2020-02-16 00:42:47 +0000

    log out button, user custom navbar and page restrictions

---
+++ src/components/headerP.js
@@ -0,0 +1,34 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <Navbar bg="dark" variant="dark">
+      <Nav className="mr-auto">
+        <Nav.Link href="/home">Home</Nav.Link>
+        <Nav.Link href="/page1">page1</Nav.Link>
+        <Nav.Link href="/page2">page2</Nav.Link>
+      </Nav>
+      <Form inline>
+        // add login name
+      </Form>
+    </Navbar>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
